ff367d7fa9ce055f57a5871fc1dff7d0bccb0645
Jennifer Shannon <jen.shannon@gmail.com>
2016-06-17 11:10:50 -0400

    fix app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var promise = require('bluebird');
+var path = require('path');
+
 var app = express();
 
 var options =  {
@@ -10,7 +12,10 @@ var pgp = require('pg-promise')(options);
 
 var db = pgp('postgres://localhost:5432/instapics');
 
-app.get("/users",function(req,res,next){
+app.set('view engine','hbs');
+app.set('views', path.join(__dirname,'views'));
+
+app.get('/users',function(req,res,next){
 	db.any('SELECT * FROM users')
 	.then(function(data){
 		res.render('index',{ data: data });
